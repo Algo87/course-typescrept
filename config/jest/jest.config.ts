0c5c9516e7f099e89a516ea03b87864a896b1b7f
golovko <webcom.otp.024@gmail.com>
2024-09-27 22:53:45 +0300

    add jest test Button and Sidebar component

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -44,6 +44,7 @@ export default {
         '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
     ],
     rootDir: '../../',
+    setupFilesAfterEnv: ['<rootDir>config/jest/setUpTests.ts'],
     // setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
     moduleNameMapper: {
         '\\.s?css$': 'identity-obj-proxy',
