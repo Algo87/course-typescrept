c04268489355afaa830f03b0f2fe547db648aeb5
golovko <webcom.otp.024@gmail.com>
2024-09-29 22:15:35 +0300

    fix webpack for storybook, add new script for screeshot testing

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -12,11 +12,12 @@ export default ({ config }: { config: webpack.Configuration }) => {
 
     };
 
-    config.resolve.modules.push(paths.src);
-    config.resolve.extensions.push('.tsx', '.ts', '.js');
+    config!.resolve!.modules!.push(paths.src);
+    config!.resolve!.extensions!.push('.tsx', '.ts', '.js');
 
     // eslint-disable-next-line no-param-reassign
-    config!.module!.rules = config.module!.rules!.map((rule: RuleSetRule) => {
+    // @ts-ignore
+    config!.module!.rules = config!.module!.rules!.map((rule: RuleSetRule) => {
         if (/svg/.test(rule.test as string)) {
             return { ...rule, exclude: /\.svg$/i };
         }
